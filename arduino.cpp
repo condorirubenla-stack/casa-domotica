60369422eb69de4ee9b1fba2624f780c86e1b887
condorirubenla-stack <condorirubenla@gmail.com>
2026-05-01 16:56:03 -0400

    Add files via upload

---
+++ arduino.cpp
@@ -0,0 +1,36 @@
+#include <WiFi.h>
+#include <WebServer.h>
+
+// Credenciales de tu red
+const char* ssid = "FLIA_CONDORI";
+const char* password = "27071988"; 
+
+WebServer server(80);
+
+// Pines: 4 Cuartos, 3 Sala, 1 Ventilador
+const int pins[8] = {13, 12, 14, 27, 26, 25, 33, 32};
+
+void setup() {
+  Serial.begin(115200);
+  for(int i=0; i<8; i++) { pinMode(pins[i], OUTPUT); digitalWrite(pins[i], LOW); }
+  
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
+  
+  Serial.println("\n--- SISTEMA EDUCONECT LISTO ---");
+  Serial.print("IP Local para la App: ");
+  Serial.println(WiFi.localIP());
+
+  // Ruta para recibir los comandos del teléfono
+  server.on("/toggle", [](){
+    server.sendHeader("Access-Control-Allow-Origin", "*"); 
+    if (server.hasArg("id")) {
+      int id = server.arg("id").toInt();
+      digitalWrite(pins[id], !digitalRead(pins[id]));
+    }
+    server.send(200, "text/plain", "OK");
+  });
+  server.begin();
+}
+
+void loop() { server.handleClient(); }
